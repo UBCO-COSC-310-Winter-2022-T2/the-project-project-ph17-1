8b7bad90b7c284b8e8676bbadcad2080b087c7f6
Lydia108 <112993443+Lydia108@users.noreply.github.com>
2023-04-01 22:49:09 -0700

    Update handleorder.php

---
+++ code/php/handleorder.php
@@ -0,0 +1,33 @@
+class OrderHandler
+{
+    private $orders = [];
+
+    public function addOrder($orderId, $status)
+    {
+        $this->orders[$orderId] = $status;
+    }
+
+    public function getOrderStatus($orderId)
+    {
+        if (array_key_exists($orderId, $this->orders)) {
+            return $this->orders[$orderId];
+        } else {
+            return null;
+        }
+    }
+
+    public function deleteOrder($orderId)
+    {
+        unset($this->orders[$orderId]);
+    }
+
+    public function updateStatus($orderId, $newStatus)
+    {
+        if (array_key_exists($orderId, $this->orders)) {
+            $this->orders[$orderId] = $newStatus;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
