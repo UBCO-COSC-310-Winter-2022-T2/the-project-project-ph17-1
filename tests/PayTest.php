c22105037fc100bf1528ebc27073a383e29f14b7
JiajunJerryHuang <77290113+JiajunJerryHuang@users.noreply.github.com>
2023-04-03 20:39:25 -0700

    Update PayTest.php

--- tests/PayTest.php
+++ tests/PayTest.php
@@ -29,41 +29,6 @@ class PaymentTest extends TestCase
     }
 }
 ?>
-<?php
-// File: code/php/pay.php
-
-class Payment
-{
-    private $payments;
-
-    public function __construct()
-    {
-        $this->payments = [];
-    }
-
-    public function addPayment($amount, $currency)
-    {
-        $paymentId = count($this->payments) + 1;
-
-        $this->payments[$paymentId] = [
-            'id' => $paymentId,
-            'amount' => $amount,
-            'currency' => $currency,
-        ];
-
-        return true;
-    }
-
-    public function getPaymentById($paymentId)
-    {
-        if (array_key_exists($paymentId, $this->payments)) {
-            return $this->payments[$paymentId];
-        }
-
-        return null;
-    }
-}
-?>
 
 
 
